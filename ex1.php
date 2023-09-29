5115eb7b430c5a4f123dc8c32e8a062ce864239c
pedrohqa <130668972+pedrohqa@users.noreply.github.com>
2023-09-29 09:01:52 -0300

    pedro

---
+++ ex1.php
@@ -0,0 +1,25 @@
+<?php
+    $num1 = 5;
+    $num2 = 2;
+    if($num1 > $num2){
+        echo "Essa afirmação é verdadeira $num1 é maior que $num2 <br> ";
+    }else{
+        echo "Essa afirmação é Falsa $num1 é menor que $num2 <br> ";
+    }
+
+    $nome1 = "Robson";
+    $nome2 = "Pedro";
+    if($nome1 != $nome2){
+        echo " $nome1 é diferente do $nome2 <br> ";
+    }else{
+        echo " $nome1 é igual a $nome2 <br> ";
+    }
+
+    $num3 = 12;
+    $num4 = 11;
+    if($num3 <= $num4){
+        echo "$num3 é menor ou igual a $num4";
+    }else{
+        echo "$num3 Não é menor ou igual a $num4";
+    }
+?>
