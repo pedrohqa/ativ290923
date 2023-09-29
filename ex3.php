82b8960bf687fe8ca233d3db2bf31e82672cd816
pedrohqa <130668972+pedrohqa@users.noreply.github.com>
2023-09-29 10:53:33 -0300

    p

--- ex3.php
+++ ex3.php
@@ -6,12 +6,34 @@
 
     if (is_int($num1)) {
         if($num1 > 100){
-            echo "$num1 é maior que 100";
+            echo "$num1 é maior que 100 <br>";
         }else{
-            echo "$num1 é menor que 100";
+            echo "$num1 é menor que 100 <br>";
         }
     } else {
-        echo"$num1 não é um numero";
+        echo"$num1 não é um numero <br>";
+    }
+
+
+    if (is_int($num2)) {
+        if($num2 > 100){
+            echo "$num2 é maior que 100 <br>";
+        }else{
+            echo "$num2 é menor que 100 <br>";
+        }
+    } else {
+        echo"$num2 não é um numero <br>";
+    }
+
+
+    if (is_int($num3)) {
+        if($num3 > 100){
+            echo "$num3 é maior que 100 <br>";
+        }else{
+            echo "$num3 é menor que 100 <br>";
+        }
+    } else {
+        echo"$num3 não é um numero <br>";
     }
 ?>
 
